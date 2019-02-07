5011f7e4f25f23d6d15a95b50529de577431817d
Daniel Dababneh <Daniel.Dababneh@iam.local>
2019-02-07 17:43:45 -0600

    Started special feature

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+
+    private FlashLight flashLight;
+
+
+    // Start is called before the first frame update
+     
+    void Start()
+    {
+        flashLight = GetComponent<FlashLight>();
+
+        flashLight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Flashlight is on.");
+
+            flashLight.enabled = true;
+        }
+        else if (flashLight.enabled = true && Input.GetKeyDown(KeyCode.F))
+        {
+
+            Debug.Log("Flashlight is on.");
+
+            flashLight.enabled = false;
+
+        }
+    }
+}
