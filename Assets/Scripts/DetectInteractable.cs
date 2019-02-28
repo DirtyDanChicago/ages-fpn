4c7d10cfd313112b8837ccf60913fd697380b030
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2019-02-27 18:53:10 -0600

    Add item detection script

---
+++ Assets/Scripts/DetectInteractable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /// <summary>
+    /// This script detects the interactable elements the player is looking at.
+    /// 
+    /// Raycasting Reference:
+    /// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+    /// </summary>
+
+    [SerializeField]
+    private Transform raycastOrigin;
+
+
+}
