905ef47f358398c2c6a197eda717888d72c32c4b
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2019-02-27 21:11:14 -0600

    Start interface

---
+++ Assets/Scripts/IInteractive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Interface for all the things the player can interact with.
+/// </summary>
+
+
+public interface IInteractive
+{
+
+    void InteractWith();
+
+
+}
