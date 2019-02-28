880901b97031c3247e128588c1408ac20a44e17d
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2019-02-27 19:26:03 -0600

    Raycast made

--- Assets/Scripts/DetectInteractable.cs
+++ Assets/Scripts/DetectInteractable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DetectInteractable : MonoBehaviour
 {
     /// <summary>
     /// This script detects the interactable elements the player is looking at.
@@ -11,8 +11,23 @@ public class NewBehaviourScript : MonoBehaviour
     /// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
     /// </summary>
 
+    [Tooltip ("Starting point of the raycast.")]
     [SerializeField]
     private Transform raycastOrigin;
+
+    [Tooltip("This is the maximum distance the raycast can detect interactables.")]
+    [SerializeField]
+    private float maxDistance = 5.0f;
+
+    private Vector3 raycastDirection;
+
+    private void FixedUpdate()
+    {
+        raycastDirection = raycastOrigin.forward;
+        Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, maxDistance);
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * maxDistance, Color.red);
+    }
+
 
 
 }
