b658f32057a3b4b339a83aac5b8ece1b848b47e2
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2019-04-20 15:44:22 -0500

    Start work on inventory menu display

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script provides the connection to the player's inventory menu and the player's inventory list.
+/// </summary>
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+
+    public InventoryMenu Instance
+    {
+        get
+        {
+            return Instance;
+        }
+
+        private set { instance = value; }
+    }
+}
