e839630d71be084092e3c14127c23b86b5225672
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2019-05-08 01:23:57 -0500

    Updated scenes

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -5,7 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    //Scene name entered in editor.
+    [SerializeField]
+    private string sceneToLoad;
 
+    //Loads required scene on trigger.
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
